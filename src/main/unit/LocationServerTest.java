27233ad669b0797f507bb7b56816259de7cbd557
mnskgrayce <s3751450@rmit.edu.vn>
2020-05-16 11:56:50 +0700

    -- modified LS and WA unit tests

---
+++ src/main/unit/LocationServerTest.java
@@ -0,0 +1,61 @@
+package main.unit;
+
+import main.LocationServer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedHashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LocationServerTest {
+
+    LocationServer ls;
+    Method readConfig;
+    LinkedHashMap<String, String> actual;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() throws Exception {
+        this.ls = new LocationServer();
+        readConfig = (LocationServer.class).getDeclaredMethod("readConfig");
+        readConfig.setAccessible(true);
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("LocationConfigFileShouldBeReadCorrectly")
+    public void testReadConfig() throws Exception {
+        actual = (LinkedHashMap<String, String>) readConfig.invoke(null);
+        LinkedHashMap<String, String> expectedResults = new LinkedHashMap<>();
+        expectedResults.put("A", "Indoor");
+        expectedResults.put("B", "Indoor");
+        expectedResults.put("C", "Outdoor");
+        expectedResults.put("D", "Outdoor");
+        assertEquals(expectedResults, actual);
+    }
+
+    @Test
+    @DisplayName("TestReadConfigFileWhenEmpty")
+    // LocationServerConfig must be empty
+    public void testReadConfigEmpty() throws Exception {
+        actual = (LinkedHashMap<String, String>) readConfig.invoke(null);
+        LinkedHashMap<String, String> expectedResults = new LinkedHashMap<>();
+        assertEquals(expectedResults, actual);
+    }
+
+    @Test
+    @DisplayName("TestReadConfigFileWithBadData")
+    // LocationServerConfig: "sfshsh fsfhsoa[, 121019, fwwf"
+    public void testReadConfigBadData() {
+        try {
+            actual = (LinkedHashMap<String, String>) readConfig.invoke(null);
+        } catch (ArrayIndexOutOfBoundsException | IllegalAccessException | InvocationTargetException e) {
+            assertEquals("Index 1 out of bounds for length 1", e.getMessage());
+        }
+    }
+}
---
+++ src/main/unit/WeatherAlarmsTest.java
@@ -0,0 +1,35 @@
+package main.unit;
+
+import main.WeatherAlarms;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WeatherAlarmsTest {
+
+    WeatherAlarms wa;
+    Method readWeatherConditions;
+    List<Integer> actual;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() throws NoSuchMethodException {
+        this.wa = new WeatherAlarms();
+        readWeatherConditions = (WeatherAlarms.class).getDeclaredMethod("readWeatherConditions");
+        readWeatherConditions.setAccessible(true);
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testReadWeatherConditions() throws Exception {
+        List<Integer> expected = Arrays.asList(0, 1, 2, 3);
+        actual = (List<Integer>) readWeatherConditions.invoke(wa);
+
+        assertEquals(expected, actual);
+    }
+}
